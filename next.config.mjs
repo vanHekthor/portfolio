eec5b132eb2b9d7e40d97c5793f91993b4c583e7
Quoc Duong Bui <bui.qcd@gmail.com>
2023-02-22 11:19:41 +0100

    Comment i18n out

--- next.config.mjs
+++ next.config.mjs
@@ -18,10 +18,10 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
   assetPrefix: isProd ? '/portfolio/' : '',
   images: {
     unoptimized: true,
